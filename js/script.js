39fb1cd448a41c086210a5c2e9c4bffbd66a8fad
Frédéric Rieunier <frederic.rieunier@gmail.com>
2020-11-17 22:05:51 +0100

    Add: first version of reservation.html. Finalization of spa.html. Fixed: og images in head.

--- js/script.js
+++ js/script.js
@@ -60,3 +60,71 @@ toggleSpa($('.spa-fiche-contenu3'), $('.spa-fiche-toggle3'));
 toggleSpa($('.spa-fiche-contenu4'), $('.spa-fiche-toggle4'));
 toggleSpa($('.spa-fiche-contenu5'), $('.spa-fiche-toggle5'));
 
+
+
+
+/*  Datepicker pour le formulaire de réservation
+J'ai dû assembler différents codes pour obtenir un résultat qui fonctionne (c'est-à-dire qui n'autorise pas l'internaute à choisir une date d'arrivée antérieure à la date du jour, ni à choisir une date de départ antérieure à la date d'arrivée, ET qui soit en français).
+Je n'ai pas pu prendre directement le datepicker date-range proposé par jQuery.
+Il y a peut-être un moyen facile de l'adapter mais je ne l'ai pas trouvé.
+*/
+
+// Empêcher une date d'arrivée antérieure à la date du jour ou une date de départ antérieure à la date d'arrivée. Code trouvé ici : https://stackoverrun.com/fr/q/2161047
+
+var dateToday = new Date(); 
+var dates = $("#from, #to").datepicker({ 
+    defaultDate: "+1w", 
+    changeMonth: true, 
+    numberOfMonths: 2, 
+    minDate: dateToday, 
+    onSelect: function(selectedDate) { 
+     var option = this.id == "from" ? "minDate" : "maxDate", 
+      instance = $(this).data("datepicker"), 
+      date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings); 
+     dates.not(this).datepicker("option", option, date); 
+    } 
+}); 
+
+// Obtenir une traduction française du calendrier et un formatage français des dates (jj/mm/aaaa)
+// Code trouvé sur le Github de jQuery : https://github.com/jquery/jquery-ui/blob/master/ui/i18n/datepicker-fr.js
+
+/* French initialisation for the jQuery UI date picker plugin. */
+/* Written by Keith Wood (kbwood{at}iinet.com.au),
+			  Stéphane Nahmani (sholby@sholby.net),
+			  Stéphane Raimbault <stephane.raimbault@gmail.com> */
+         ( function( factory ) {
+          if ( typeof define === "function" && define.amd ) {
+        
+            // AMD. Register as an anonymous module.
+            define( [ "../widgets/datepicker" ], factory );
+          } else {
+        
+            // Browser globals
+            factory( jQuery.datepicker );
+          }
+        }( function( datepicker ) {
+        
+        datepicker.regional.fr = {
+          closeText: "Fermer",
+          prevText: "Précédent",
+          nextText: "Suivant",
+          currentText: "Aujourd'hui",
+          monthNames: [ "janvier", "février", "mars", "avril", "mai", "juin",
+            "juillet", "août", "septembre", "octobre", "novembre", "décembre" ],
+          monthNamesShort: [ "janv.", "févr.", "mars", "avr.", "mai", "juin",
+            "juil.", "août", "sept.", "oct.", "nov.", "déc." ],
+          dayNames: [ "dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
+          dayNamesShort: [ "dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam." ],
+          dayNamesMin: [ "D","L","M","M","J","V","S" ],
+          weekHeader: "Sem.",
+          dateFormat: "dd/mm/yy",
+          firstDay: 1,
+          isRTL: false,
+          showMonthAfterYear: false,
+          yearSuffix: "" };
+        datepicker.setDefaults( datepicker.regional.fr );
+        
+        return datepicker.regional.fr;
+        
+        } ) );
+
